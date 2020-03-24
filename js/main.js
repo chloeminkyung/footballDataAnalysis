90c74149dad56c08e23b067011ac52a47e3a1594
Chloe Kim <chloeminkyung@gmail.com>
2020-03-24 21:11:05 +0800

    update

--- js/main.js
+++ js/main.js
@@ -754,6 +754,13 @@
   }
 
   function init () {
+    $.ajaxPrefilter( function (options) {
+      if (options.crossDomain && jQuery.support.cors) {
+        var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+        options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+      }
+    });
+    
     initNavbar();
     initHomeSlider();
     initCarousels();
